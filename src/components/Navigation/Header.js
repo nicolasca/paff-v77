b7da007338ffaeca177ace6865179cfb795bd10c
nicolasca <castejon.nicolas@gmail.com>
2019-06-22 21:42:55 +0200

    Ajouter activeClass pour la navigation en cours

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from './../../assets/logo.jpg'
 
@@ -8,11 +8,19 @@ function Header() {
   return (
     <header className={styles.Header}>
       <div className={styles.Logo}>
-        <img src={logo} alt="logo"/>
+        <img src={logo} alt="logo" />
       </div>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/deckbuilder">Deck</Link></li>
+        <li><NavLink
+          exact to="/"
+          activeClassName={styles.ActiveNavLink}>
+          Home
+        </NavLink></li>
+        <li><NavLink
+          exact to="/deckbuilder"
+          activeClassName={styles.ActiveNavLink}>
+          Deck
+        </NavLink></li>
       </ul>
     </header>
 
