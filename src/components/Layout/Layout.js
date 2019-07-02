1b89f6c40c6e03eb894d44a061d29454d83d202f
nicolasca <castejon.nicolas@gmail.com>
2019-07-02 20:08:44 +0200

    Restreindre le site uniquement aux utilisateurs authentifiés

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import DeckBuilder from '../DeckBuilder/DeckBuilder';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Home from '../Home/Home';
 import Header from '../Navigation/Header';
@@ -16,28 +16,49 @@ import Logout from '../Auth/Logout/Logout';
 
 function Layout(props) {
 
+  let content = (
+    <main className={styles.Main}>
+      <Route path="/auth" exact component={Auth} />
+      <Route path="/signin" exact component={SignIn} />
+      <Redirect to="/auth"></Redirect>
+    </main>
+  );
+
+  if (props.isAuthenticated) {
+    content = (
+      <React.Fragment>
+        <Header
+          className={styles.Header}
+          username={props.username}
+          isAuthenticated={props.isAuthenticated}></Header>
+        <main className={styles.Main}>
+          <Route path="/home" component={Home} />
+          <Route path="/" exact component={Home} />
+          <Route path="/logout" exact component={Logout} />
+          <Route path="/auth" exact component={Auth} />
+          <Route path="/signin" exact component={SignIn} />
+          <Route path="/deck" exact component={DeckBuilder} />
+          <Route path="/factions" exact component={Factions} />
+          <Route path="/factions/peaux-vertes" exact component={FactionPeauxVertes} />
+          <Route path="/factions/sephosi" exact component={FactionSephosi} />
+          <Route path="/factions/gaeli" exact component={FactionGaeli} />
+          <Route path="/factions/liches" exact component={FactionLiches} />
+          <Redirect to="/"></Redirect>
+
+        </main>
+      </React.Fragment>
+    );
+  }
+
+
+
   return (
     <div className={styles.Site}>
-      <Header
-        className={styles.Header}
-        username={props.username}
-        isAuthenticated={props.isAuthenticated}></Header>
-      <main className={styles.Main}>
-        <Route path="/home" component={Home} />
-        <Route path="/" exact component={Home} />
-        <Route path="/logout" exact component={Logout} />
-        <Route path="/auth" exact component={Auth} />
-        <Route path="/signin" exact component={SignIn} />
-        <Route path="/deck" exact component={DeckBuilder} />
-        <Route path="/factions" exact component={Factions} />
-        <Route path="/factions/peaux-vertes" exact component={FactionPeauxVertes} />
-        <Route path="/factions/sephosi" exact component={FactionSephosi} />
-        <Route path="/factions/gaeli" exact component={FactionGaeli} />
-        <Route path="/factions/liches" exact component={FactionLiches} />
-      </main>
+      {content}
     </div>
   );
 }
+
 
 const mapStateToProps = state => {
   return {
