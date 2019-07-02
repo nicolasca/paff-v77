35516c485fd680fbd3021d3b34324a197c108f67
nicolasca <castejon.nicolas@gmail.com>
2019-07-02 20:50:09 +0200

    Ajouter les bg pour Sephosi et Liches

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from './Home.module.css'
+import styles from './Home.module.css';
+import logo from '../../assets/logo.jpg';
 
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
   return (
     <div className={styles.MainPage}>
       <h1>PAFF v77</h1>
+      <img src={logo} alt="" />
     </div>
   );
 }
