961e14cb76d6c0bffbbaff5af3769d82f3af9cc8
ncastejon <castejon.nicolas@gmail.com>
2020-02-10 13:57:19 +0100

    Refacto the faction page. Remove duplicated pages.

--- src/components/Factions/Factions.tsx
+++ src/components/Factions/Factions.tsx
@@ -1,50 +1,11 @@
 import React, { FunctionComponent } from "react";
+import { config } from "../../config";
 import { IFaction } from "../../models/IFaction";
 import { FactionService } from "../../services/Faction.service";
-import FactionGaeli from "./Faction/FactionGaeli";
-import FactionLiches from "./Faction/FactionLiches";
-import FactionPeauxVertes from "./Faction/FactionPeauxVertes";
-import FactionSephosi from "./Faction/FactionSephosi";
 import styles from "./Factions.module.scss";
 
 const Factions: FunctionComponent = () => {
   const [factions, setFactions] = React.useState<IFaction | null>(null);
-
-  // React.useEffect(() => {
-  //   axios.get(config.host + ":3008/factions").then(response => {
-  //     setFactions(
-  //       response.data.map((faction: IFaction) => {
-  //         let factionComponent = null;
-
-  //         if (faction.slug === "peaux-vertes") {
-  //           factionComponent = (
-  //             <FactionPeauxVertes
-  //               key="PV"
-  //               faction={faction}
-  //             ></FactionPeauxVertes>
-  //           );
-  //         }
-  //         if (faction.slug === "gaeli") {
-  //           factionComponent = (
-  //             <FactionGaeli key="gaeli" faction={faction}></FactionGaeli>
-  //           );
-  //         }
-  //         if (faction.slug === "sephosi") {
-  //           factionComponent = (
-  //             <FactionSephosi key="sephosi" faction={faction}></FactionSephosi>
-  //           );
-  //         }
-  //         if (faction.slug === "liches") {
-  //           factionComponent = (
-  //             <FactionLiches key="liches" faction={faction}></FactionLiches>
-  //           );
-  //         }
-
-  //         return factionComponent;
-  //       })
-  //     );
-  //   });
-  // }, []);
 
   React.useEffect(() => {
     FactionService.getFactions()
@@ -53,72 +14,60 @@ const Factions: FunctionComponent = () => {
         setFactions(
           factions.data.map((faction: IFaction) => {
             let factionComponent = null;
+            const backgroundImage = config.directus + config.directus_files + faction.image.filename_disk;
 
-            if (faction.slug === "peaux-vertes") {
-              factionComponent = (
-                <FactionPeauxVertes
-                  key="PV"
-                  faction={faction}
-                ></FactionPeauxVertes>
-              );
-            }
-            if (faction.slug === "gaeli") {
-              factionComponent = (
-                <FactionGaeli key="gaeli" faction={faction}></FactionGaeli>
-              );
-            }
-            if (faction.slug === "sephosi") {
-              factionComponent = (
-                <FactionSephosi key="sephosi" faction={faction}></FactionSephosi>
-              );
-            }
-            if (faction.slug === "liches") {
-              factionComponent = (
-                <FactionLiches key="liches" faction={faction}></FactionLiches>
-              );
-            }
+            factionComponent = (
+              <div className={styles.Faction + ' ' + styles[faction.slug]}>
+                {faction ?
+                  <React.Fragment>
+                    <div style={{ backgroundImage: `url(${backgroundImage})` }}
+                      className={styles.BgImage}
+                      title={faction.name} />
+                    <div className={styles.Description}>
+                      <h2 className={styles.Title}>{faction.name}</h2>
+                      <div className={styles.Lettrine}
+                        dangerouslySetInnerHTML={{ __html: faction.description }} />
+                    </div>
+
+                  </React.Fragment> : null}
+              </div>
+            )
+
+            // if (faction.slug === "peaux-vertes") {
+            //   factionComponent = (
+            //     <FactionPeauxVertes
+            //       key="PV"
+            //       faction={faction}
+            //     ></FactionPeauxVertes>
+            //   );
+            // }
+            // if (faction.slug === "gaeli") {
+            //   factionComponent = (
+            //     <FactionGaeli key="gaeli" faction={faction}></FactionGaeli>
+            //   );
+            // }
+            // if (faction.slug === "sephosi") {
+            //   factionComponent = (
+            //     <FactionSephosi key="sephosi" faction={faction}></FactionSephosi>
+            //   );
+            // }
+            // if (faction.slug === "liches") {
+            //   factionComponent = (
+            //     <FactionLiches key="liches" faction={faction}></FactionLiches>
+            //   );
+            // }
 
             return factionComponent;
           })
         );
       });
-
-    // axios.get(config.host + ":3008/factions").then(response => {
-    //   setFactions(
-    //     response.data.map((faction: IFaction) => {
-    //       let factionComponent = null;
-
-    //       if (faction.slug === "peaux-vertes") {
-    //         factionComponent = (
-    //           <FactionPeauxVertes
-    //             key="PV"
-    //             faction={faction}
-    //           ></FactionPeauxVertes>
-    //         );
-    //       }
-    //       if (faction.slug === "gaeli") {
-    //         factionComponent = (
-    //           <FactionGaeli key="gaeli" faction={faction}></FactionGaeli>
-    //         );
-    //       }
-    //       if (faction.slug === "sephosi") {
-    //         factionComponent = (
-    //           <FactionSephosi key="sephosi" faction={faction}></FactionSephosi>
-    //         );
-    //       }
-    //       if (faction.slug === "liches") {
-    //         factionComponent = (
-    //           <FactionLiches key="liches" faction={faction}></FactionLiches>
-    //         );
-    //       }
-
-    //       return factionComponent;
-    //     })
-    //   );
-    // });
   }, []);
 
-  return <div className={styles.Faction}>{factions}</div>;
+  
+
+  return (
+  <div className={styles.Faction}>{factions}</div>
+  );
 };
 
 export default Factions;
