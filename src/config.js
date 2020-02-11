73c4a1b3c0191cb4042290dbbbf6520696d6eeb7
nicolasca <castejon.nicolas@gmail.com>
2020-02-11 20:35:16 +0100

    Change host url and add bind id for the docker ports.

--- src/config.js
+++ src/config.js
@@ -1,32 +1,31 @@
-
 export const config = {
-    host: 'http://51.75.252.210',
-    // host: 'http://localhost',
-    typeCard: {
-        ordre: {
-            label: 'ordre',
-        },
-        troupe: {
-            label: 'troupe',
-        },
-        tir: {
-            label: 'tir',
-        },
-        cavalerie: {
-            label: 'cavalerie',
-            limite: 6,
-        },
-        artillerie: {
-            label: 'artillerie',
-            limite: 4,
-        },
-        elite: {
-            label: 'elite',
-            limite: 1,
-        },
-        unique: {
-            label: 'unique',
-            limite: 1,
-        },
+  host: "https://paff.nicolasca.com",
+  // host: 'http://localhost',
+  typeCard: {
+    ordre: {
+      label: "ordre"
+    },
+    troupe: {
+      label: "troupe"
+    },
+    tir: {
+      label: "tir"
+    },
+    cavalerie: {
+      label: "cavalerie",
+      limite: 6
+    },
+    artillerie: {
+      label: "artillerie",
+      limite: 4
+    },
+    elite: {
+      label: "elite",
+      limite: 1
+    },
+    unique: {
+      label: "unique",
+      limite: 1
     }
-}
+  }
+};
