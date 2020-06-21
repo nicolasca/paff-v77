0b46d47e984526a2b2a80dcc6eb8e2dd0f921c20
nicolasca <castejon.nicolas@gmail.com>
2020-06-21 18:52:01 +0200

    TS erreor

--- src/components/Game/Board/Reserve/Reserve.js
+++ src/components/Game/Board/Reserve/Reserve.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import styles from './Reserve.module.scss';
+import React from "react";
+import styles from "./Reserve.module.scss";
 
 function Reserve(props) {
-
-
   return (
     <React.Fragment>
-
       <div className={styles.Reserve}>
-        <div className={`${styles.Hand} `} >
-          {props.topHand}
-        </div>
+        <div className={`${styles.Hand} `}>{props.topHand}</div>
 
-        <div className={`${styles.Other}`} >
+        <div className={`${styles.Other}`}>
           Votre adversaire a {props.bottomHand.length} cartes dans sa réserve
         </div>
       </div>
-
-      )
-  }
-        </React.Fragment>
-
+      ) }
+    </React.Fragment>
   );
 }
 
